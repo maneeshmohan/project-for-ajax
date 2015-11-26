1507132c5119282a604a00854b38d52fd62d24f4
maneeshmohan <mmaneesh619@gmail.com>
2015-11-26 17:08:54 +0530

    "commit at 15.08"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ponies::Application.config.secret_key_base = 'b0e4720093e899619be2990ab015bb024b0ac8b1c6aa06078d2b6f1bf44d36460734d306e69ce70809f08cd7f6c83773a62c1d6e68ae1ee3206fe96403368be5'
